37ecad1d2e99a40888f09ff6ad389731cc3d0115
kipkirui63 <enocksang8356@gmail.com>
2023-07-27 17:44:04 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,14 +3,12 @@ import { actors } from "../data";
 
 
 
-
-
 function Actors() {
   return (
     <div>
       <h1>Actors Page</h1>
       {actors.map((actor, index) => (
-        <div>
+        <div key={index}>
           <h2>{actor.name}</h2>
           <ul>
             {actor.movies.map((movie, index) =>(
